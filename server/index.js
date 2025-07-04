1a2b168f3ddd55322236007599ca25924d937526
ANAND KUMAR <georgian3125anand@gmail.com>
2025-07-05 00:59:36 +0530

    originchange

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   // origin: "http://localhost:5173",
-  origin: "http://  lms-zq1s.onrender.com",
+  origin: "http://lms-zq1s.onrender.com",
 
 
 
